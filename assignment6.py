d44af30d864024d6d18134f87bc6795f62c52bc9
jterstall <jeroen_t@hotmail.com>
2016-10-13 16:59:55 +0200

    text per class

--- assignment6.py
+++ assignment6.py
@@ -1,19 +1,13 @@
 from __future__ import division
-from bs4 import BeautifulSoup
-from collections import Counter
 from nltk.corpus import stopwords
+from collections import Counter
 import pandas as pd
-import MySQLdb
 import string
 import nltk
-import glob
-import os
-import re
 
 # Tycho Koster, David Stap, Jeroen Terstall
 
 if __name__ == '__main__':
-    folder = "doc/KVR/"
     kvrdf= pd.read_csv('KVR.csv.gz',
                    sep='\t', encoding='utf-8',
                    index_col=0,
@@ -22,7 +16,6 @@ if __name__ == '__main__':
 
     # Join all titels of the questions together
     allvragen = '\n'.join(str(v) for v in list(kvrdf.titel))
-
     # remove punctuation
     allvragen = allvragen.translate(str.maketrans({key: None for key in string.punctuation}))
 
@@ -31,24 +24,23 @@ if __name__ == '__main__':
     # show the top 20 most used words
     BoW.most_common(20)
 
-    DutchStop= stopwords.words('dutch')
+    DutchStop = stopwords.words('dutch')
 
-    BoW= BoW= Counter([w for w in nltk.word_tokenize(allvragen) if not w in set(DutchStop)])
+    BoW = Counter([w for w in nltk.word_tokenize(allvragen) if not w in set(DutchStop)])
     # show the top 20 most used words
     BoW.most_common(20)
 
-    V = allvragen
-    Nc = kvrdf.ministerie.value_counts().head(10)
-    i=0
+
+    V = list(BoW)
     conprob = {}
+    # Concatenate text
+    text = []
     for c in range(len(normalized_ministerie)):
+        text.append([])
         prior_c = normalized_ministerie[c]
-        Nct = 0
-        print(prior_c)
-        for t in V:
-            for d in range(len(kvrdf.titel)):
-                if(kvrdf.ministerie.keys()[c] == kvrdf.ministerie[d]):
-                    if(t in kvrdf.titel or t in kvrdf.vraag or t in kvrdf.antwoord):
-                        Nct +=1
-            conprob[kvrdf.ministerie.keys()[c]][t] = ((Nct+1)/(Nc[c] +2))
-    print(conprob)
+        ministerie = kvrdf.ministerie.value_counts().keys()[c]
+        for d in range(len(kvrdf.ministerie)):
+            if(ministerie == kvrdf.ministerie[d]):
+                text_document = kvrdf.titel[d].translate(str.maketrans({key: None for key in string.punctuation}))
+                text[c].append(text_document)
+    print([Counter(nltk.word_tokenize(text_c)) for text_c in text])
