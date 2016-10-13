12773aa030980e2864348a285713333e733070e2
jterstall <jeroen_t@hotmail.com>
2016-10-13 14:21:49 +0200

    Ipython -> python

--- assignment6.py
+++ assignment6.py
@@ -1,26 +1,54 @@
 from __future__ import division
 from bs4 import BeautifulSoup
 from collections import Counter
+from nltk.corpus import stopwords
+import pandas as pd
 import MySQLdb
+import string
+import nltk
 import glob
 import os
 import re
 
 # Tycho Koster, David Stap, Jeroen Terstall
 
-def index_collection(folder):
-    ministerie_lijst = []
-    for infile in glob.glob( os.path.join(folder, '*.xml') ): # loop over each file
-        with open(infile, 'r') as f:  # open file
-            soup = BeautifulSoup(f.read(), "html.parser")  # get the text out
-            items = soup.find_all("item", {"attribuut" : "Afkomstig_van"})
-            for i in items:
-                # Normalize by removing the abbreviations between parentheses
-                ministerie = re.sub('\(.*?\)','', i.string).rstrip()
-                ministerie_lijst.append(ministerie)
-    return Counter(ministerie_lijst)
-
 if __name__ == '__main__':
     folder = "doc/KVR/"
-    counter = index_collection(folder)
-    print counter
+    kvrdf= pd.read_csv('KVR.csv.gz',
+                   sep='\t', encoding='utf-8',
+                   index_col=0,
+                   names=['jaar', 'partij','titel','vraag','antwoord','ministerie'])
+    normalized_ministerie = kvrdf.ministerie.value_counts().head(10) / sum(kvrdf.ministerie.value_counts())
+
+    # Join all titels of the questions together
+    allvragen = '\n'.join(str(v) for v in list(kvrdf.titel))
+
+    # remove punctuation
+    allvragen = allvragen.translate(str.maketrans({key: None for key in string.punctuation}))
+
+    # Tokenize and turn into a bag of words
+    BoW = Counter(nltk.word_tokenize(allvragen))
+    # show the top 20 most used words
+    BoW.most_common(20)
+
+    DutchStop= stopwords.words('dutch')
+
+    BoW= BoW= Counter([w for w in nltk.word_tokenize(allvragen) if not w in set(DutchStop)])
+    # show the top 20 most used words
+    BoW.most_common(20)
+
+    V = allvragen
+    Nc = kvrdf.ministerie.value_counts().head(10)
+    i=0
+    conprob = {}
+    for c in range(len(normalized_ministerie)):
+        prior_c = normalized_ministerie[c]
+        Nct = 0
+        print(prior_c)
+        for t in V:
+            for d in range(len(kvrdf.titel)):
+                if(kvrdf.ministerie.keys()[c] == kvrdf.ministerie[d]):
+                    if(t in kvrdf.titel or t in kvrdf.vraag or t in kvrdf.antwoord):
+                        Nct +=1
+            conprob[kvrdf.ministerie.keys()[c]][t] = ((Nct+1)/(Nc[c] +2))
+    print(conprob)
